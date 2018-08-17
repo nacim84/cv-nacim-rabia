22de5c975f9a0bfd9e77597332562b59751335cc
nacim84 <rabia.nacim@yahoo.com>
2018-08-17 14:49:42 +0200

    Add the differents components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { FormationsComponent } from './formations/formations.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { OthersComponent } from './others/others.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'experiences', component: ExperiencesComponent},
+  {path: 'formations', component: FormationsComponent},
+  {path: 'competences', component: CompetencesComponent},
+  {path: 'others', component: OthersComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExperiencesComponent,
+    FormationsComponent,
+    CompetencesComponent,
+    OthersComponent,
+    MainNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formations/formations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cv-app-formations',
+  templateUrl: './formations.component.html',
+  styleUrls: ['./formations.component.css']
+})
+export class FormationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
